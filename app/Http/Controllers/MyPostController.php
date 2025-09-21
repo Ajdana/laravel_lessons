2ccd87825610ddec50a33114eb48f30cacbae7f7
Aidana <ajdanaamirtaj@gmail.com>
2025-09-21 20:42:51 +0500

    nineTeenLesson

--- app/Http/Controllers/MyPostController.php
+++ app/Http/Controllers/MyPostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class MyPostController extends Controller
@@ -14,9 +15,9 @@ class MyPostController extends Controller
 //            dump($post->title);
 //        }
 //        dd('end');
-        $category = Category::find(1);
         $post = Post::find(1);
-        dd($post->category);
+        $tag = Tag::find(1);
+        dd($tag->posts);
 //        return view('post.index', compact('posts'));
     }
 
