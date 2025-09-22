30ad2e5da9aeb8cdb32c3f499eff6497ba04f55f
Aidana <ajdanaamirtaj@gmail.com>
2025-09-22 21:36:31 +0500

    twentyFourLesson

--- app/Http/Controllers/MyPostController.php
+++ app/Http/Controllers/MyPostController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
-use App\Models\PostTag;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
@@ -12,7 +11,10 @@ class MyPostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $post = Post::find(1);
+        $category = Category::find(1);
+        $tag = Tag::find(1);
+        dd($post->category);
 //        foreach ($posts as $post) {
 //            dump($post->title);
 //        }
