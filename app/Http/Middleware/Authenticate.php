03c0a420039294f1d8e2edae758b606027d66ded
Aidana <ajdanaamirtaj@gmail.com>
2025-10-01 20:32:47 +0500

    Resolve #6 thirtyEightLesson

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
+use Closure;
 class Authenticate extends Middleware
 {
     /**
@@ -18,4 +18,12 @@ class Authenticate extends Middleware
             return route('login');
         }
     }
+
+//    public function handle($request, Closure $next, ...$guards)
+//    {
+//        $this->authenticate($request, $guards);
+//        dd($next);
+//        return $next($request);
+//
+//    }
 }
